ac41e1ea12dfcf5eb4f14063698282fcec86f52f
Innoversa <sicongjaxhuang@gmail.com>
2025-06-11 14:47:50 -0500

    implemented the data loading

--- load_24h_cgmacros.py
+++ load_24h_cgmacros.py
@@ -11,59 +11,17 @@ from sklearn.model_selection import train_test_split
 import cv2
 from tqdm import tqdm
 
-label_cols = ["Calories", "Carbs", "Protein", "Fat", "Fiber"]
-img_cols = ["Image path"]
-
-
-# TODO: complete this function
-class CGMacros24HDataset(Dataset):
-    def __init__(
-        self,
-        cgm_trace,  # CGM features in multimendional array
-        znorm_cgm,  # z-normalized CGM features
-        img_arr,
-        fasting_glucose,  # fasting glucose values
-        label_arr,  # macronutrient labels
-        transform=None,  # transform function to apply on CGM features
-    ):
-        self.cgm_trace = cgm_trace
-        self.znorm_cgm = znorm_cgm
-        self.img_arr = img_arr
-        self.fasting_glucose = fasting_glucose
-        self.label_arr = label_arr
-        self.label_cols = label_cols
-        if transform is None:
-            self.transform = self.zscore_transform
-        else:
-            self.transform = transform
-
-    def __len__(self):
-        return len(self.cgm_trace)
-
-    def __getitem__(self, idx):
-        cgm_trace = self.cgm_trace[idx]
-        znorm_cgm = self.znorm_cgm[idx]
-        img_arr = self.img_arr[idx]
-        a1c = self.a1c[idx]
-        fasting_glucose = self.fasting_glucose[idx]
-        label_arr = self.label_arr[idx]
-
-        if self.transform:
-            # cgm_trace = self.transform(cgm_trace)
-            img_arr = self.transform(img_arr)
-        return (
-            img_arr,
-            cgm_trace,
-            znorm_cgm,
-            label_arr,
-            self.label_cols,
-        )
-
-    def zscore_transform(self, orig_feat):
-        mean = np.nanmean(orig_feat, axis=1, keepdims=True)
-        std = np.nanstd(orig_feat, axis=1, keepdims=True) + 1e-8
-        norm_feat = (orig_feat - mean) / (std + 1e-8)
-        return norm_feat
+meal_event_cols = [
+    "Calories",
+    "Carbs",
+    "Protein",
+    "Fat",
+    "Fiber",
+    "Image path",
+    "Meal Type",
+]
+time_series_cols = ["Libre GL", "Dexcom GL", "HR"]
+img_size: tuple = (112, 112)
 
 
 # NOTE: Not used yet, as this is 24H experiment
@@ -92,6 +50,7 @@ def load_CGMacros(
         raise FileNotFoundError
     dataset_df = pd.read_csv(subject_file, index_col=None)
     dataset_df["Timestamp"] = pd.to_datetime(dataset_df["Timestamp"])
+    dataset_df = dataset_df.drop("Unnamed: 0", axis=1, errors="ignore")
     return dataset_df.set_index("Timestamp")
 
 
@@ -133,28 +92,6 @@ def extract_meal_trace(
     return trace_df
 
 
-def load_cgm_trace(
-    dataset_df: pd.DataFrame,
-    valid_meal_idx: pd.DatetimeIndex,
-    prev_hours: int = 0,
-    post_hours: int = 3,
-    cgm_cols=["Dexcom GL", "Libre GL"],  # Whether to select both of either one
-):
-    cgm_len = 60 * (prev_hours + post_hours)
-    cgm_trace = np.zeros((len(valid_meal_idx), len(cgm_cols), cgm_len))
-    for i, ts_idx in enumerate(valid_meal_idx):
-        # NOTE: Extracting the time-series trace for meals
-        trace_df = extract_meal_trace(dataset_df, ts_idx, prev_hours, post_hours).copy()
-        cgm_arr = trace_df[cgm_cols].T.to_numpy()
-        # NOTE: Padding the CGM features with NaNs
-        if cgm_arr.shape[1] < cgm_len:
-            padding = np.full((len(cgm_cols), cgm_len - cgm_arr.shape[1]), np.nan)
-            cgm_arr = np.concatenate((cgm_arr, padding), axis=1)
-        # NOTE: Adding features to samples
-        cgm_trace[i] = cgm_arr
-    return cgm_trace
-
-
 def get_image(
     img_filename: str,
     subject_id: int,
@@ -186,108 +123,63 @@ def load_img_data(
     return img_data
 
 
-def split_dataset(
-    cgm_trace: np.ndarray,
-    znorm_cgm: np.ndarray,
-    img_arr: np.ndarray,
-    label_arr: pd.DataFrame,
+def get_split_pids(
+    unique_subjects: list,
     test_size: float = 0.2,
     val_size: float = 0.2,
     random_state: int = 2025,
 ):
-    # NOTE: Drop rows with NaN values in cgm_trace, img_arr, or label_arr
-    valid_idx = ~(
-        np.isnan(cgm_trace).any(axis=(1, 2))
-        | np.isnan(znorm_cgm).any(axis=(1, 2))
-        | np.isnan(img_arr).any(axis=(1, 2, 3))
-        | np.isnan(label_arr).any(axis=1)
-    )
-    cgm_trace = cgm_trace[valid_idx]
-    znorm_cgm = znorm_cgm[valid_idx]
-    img_arr = img_arr[valid_idx]
-    label_arr = label_arr[valid_idx]
-
     # First split into train+val and test
-    train_val_idx, test_idx = train_test_split(
-        np.arange(len(cgm_trace)),
+    train_val_pids, test_pids = train_test_split(
+        unique_subjects,
         test_size=test_size,
         random_state=random_state,
     )
 
     # Further split train+val into train and validation
-    train_idx, val_idx = train_test_split(
-        train_val_idx,
+    train_pids, val_pids = train_test_split(
+        train_val_pids,
         test_size=val_size
         / (1 - test_size),  # Adjust validation size relative to train+val
         random_state=random_state,
     )
-
-    # Split the data into train, validation, and test sets
-    train_cgm_trace = cgm_trace[train_idx]
-    val_cgm_trace = cgm_trace[val_idx]
-    test_cgm_trace = cgm_trace[test_idx]
-
-    train_znorm_cgm = znorm_cgm[train_idx]
-    val_znorm_cgm = znorm_cgm[val_idx]
-    test_znorm_cgm = znorm_cgm[test_idx]
-
-    train_img_arr = img_arr[train_idx]
-    val_img_arr = img_arr[val_idx]
-    test_img_arr = img_arr[test_idx]
-
-    train_label_arr = label_arr[train_idx]
-    val_label_arr = label_arr[val_idx]
-    test_label_arr = label_arr[test_idx]
-
-    # Create datasets
-    train_dataset = CGMacros24HDataset(
-        cgm_trace=train_cgm_trace,
-        znorm_cgm=train_znorm_cgm,
-        img_arr=train_img_arr,
-        label_arr=train_label_arr,
-    )
-    val_dataset = CGMacros24HDataset(
-        cgm_trace=val_cgm_trace,
-        znorm_cgm=val_znorm_cgm,
-        img_arr=val_img_arr,
-        label_arr=val_label_arr,
-    )
-    test_dataset = CGMacros24HDataset(
-        cgm_trace=test_cgm_trace,
-        znorm_cgm=test_znorm_cgm,
-        img_arr=test_img_arr,
-        label_arr=test_label_arr,
-    )
-    return train_dataset, val_dataset, test_dataset
+    return train_pids, val_pids, test_pids
 
 
-def normalize_each_cgm_point(subject_cgm_arr: np.ndarray):
-    """
-    normalize_each_cgm_point normalizes the CGM data point-wise
-
-    That is, at each time point, the CGM values are normalized within a single subject across meals
-
-    Args:
-        subject_cgm_arr (np.ndarray): CGM data for a single subject
-    """
-    normalized_arr = np.zeros_like(subject_cgm_arr)
-    for i in range(subject_cgm_arr.shape[1]):  # each CGM sensor (2 total)
-        for j in range(subject_cgm_arr.shape[2]):  # each time point (180 total)
-            # NOTE: Normalizing each CGM point
-            mean = np.nanmean(subject_cgm_arr[:, i, j])
-            std = np.nanstd(subject_cgm_arr[:, i, j]) + 1e-8
-            normalized_arr[:, i, j] = (subject_cgm_arr[:, i, j] - mean) / std
-    return normalized_arr
-
-
-def generate_CGMacrosDataset(
-    img_size: tuple = (112, 112),
-    save_dir: str = "dataset/",
+def get_subset_by_pids(
+    time_series_trace_df: pd.DataFrame,
+    meal_event_df: pd.DataFrame,
+    heatmap_fnames: np.ndarray,
+    pids: list,
 ):
-    all_cgm_trace = []
-    all_img_data = []
+    # Split the data into train, validation, and test sets
+    trace = time_series_trace_df[time_series_trace_df["PID"].isin(pids)]
+    events = meal_event_df[meal_event_df["PID"].isin(pids)]
+    fnames = heatmap_fnames[pids]
+    return trace, events, fnames
+
+
+def generate_24H_CGMacros_dataset(
+    save_dir: str = "/scratch/CGMacros/continous24H_dataset/",
+    regenerate: bool = False,
+):
+    if os.path.exists(f"{save_dir}cgmacros_time_series_trace.csv") or regenerate:
+        time_series_trace_df = pd.read_csv(
+            f"{save_dir}cgmacros_time_series_trace.csv", index_col=0
+        )
+        meal_event_df = pd.read_csv(
+            f"{save_dir}cgmacros_meal_event.csv",
+            index_col=0,
+        )
+        heatmap_fnames = [
+            f"{save_dir}intensity_heatmaps/subject_{i}_heatmap.png"
+            for i in range(1, 50, 1)
+        ]
+        return time_series_trace_df, meal_event_df, heatmap_fnames
+    time_series_trace_df = pd.DataFrame()
     meal_event_df = pd.DataFrame()
-    all_znorm_cgm = []
+    heatmap_fnames = [str]
+    unique_subjects = [int]
     for i in tqdm(
         range(1, 50, 1),
         desc="Processing Subjects",
@@ -295,76 +187,45 @@ def generate_CGMacrosDataset(
     ):  # IDs are from 1 to 49
         try:
             dataset_df = load_CGMacros(subject_id=i)  # loading which subject
-            # NOTE: not used yet, as this is 24H experiment
-            # a1c, fg = get_subject_a1c_and_fg(subject_id=i)
         except FileNotFoundError:
             tqdm.write(f"Skipping Subject {i:03d}")
             continue
+        unique_subjects.append(i)
         # NOTE: getting the list of timestamps of valid meals
         valid_meal_idx = get_valid_meal_idx(
             dataset_df, meal_types=["breakfast"], ignore_x_hours=0
         )
-
         # NOTE: Extracting macronutritient labels
-        meal_event_series = dataset_df.loc[valid_meal_idx]
-        meal_event_series = meal_event_series.drop(columns=["Unnamed: 0"])
+        meal_event_series = dataset_df.loc[valid_meal_idx][meal_event_cols]
         meal_event_series["PID"] = i  # Adding subject ID to the labels
-        if "METs" not in meal_event_series.columns:
-            raise ValueError(
-                "METs column is missing in the dataset. Please check the dataset structure."
-            )
-
-        # NOTE: Extracting associated images
-        img_df = meal_event_series[img_cols]
-        img_data = load_img_data(img_df, i, target_size=img_size)
-
-        # NOTE: Extracting the time-series trace for each valid meal
-        cgm_trace = load_cgm_trace(dataset_df, valid_meal_idx)
-        znorm_cgm = normalize_each_cgm_point(cgm_trace)
-        # NOTE: Extracting the a1c and fasting glucose values as array
+        # NOTE: Extracting the time-series trace entirely
+        time_series_trace_series = dataset_df[time_series_cols]
+        time_series_trace_series["PID"] = i  # Adding subject ID to the labels
+        # NOTE: Attaching the filename of heatmap images
+        heatmap_fnames.append(f"{save_dir}intensity_heatmaps/subject_{i}_heatmap.png")
         tqdm.write(
-            f"Subject ID: {i}, CGM Features: {cgm_trace.shape}, "
-            f"Image Data: {img_data.shape}, Label Data: {meal_event_series.shape}"
+            f"Subject ID: {i}, 24H Features: {time_series_trace_series}, "
+            f"Label Data: {meal_event_series.shape}"
         )
-        all_cgm_trace.append(cgm_trace)
-        all_znorm_cgm.append(znorm_cgm)
-        all_img_data.append(img_data)
-        # TODO: Removing image data
-        pdb.set_trace()
+        time_series_trace_df = pd.concat(
+            [time_series_trace_df, time_series_trace_series]
+        )
         meal_event_df = pd.concat([meal_event_df, meal_event_series])
 
-    all_cgm_trace = np.concatenate(all_cgm_trace, axis=0)
-    all_znorm_cgm = np.concatenate(all_znorm_cgm, axis=0)
-    all_img_data = np.concatenate(all_img_data, axis=0)
-    all_a1c = np.concatenate(all_a1c, axis=0)
-    all_fasting_glucose = np.concatenate(all_fasting_glucose, axis=0)
-    all_label_arr = np.concatenate(all_label_arr, axis=0)
-
-    print(f"All CGM Features: {all_cgm_trace.shape}")
-    print(f"All Z-Norm CGM Features: {all_znorm_cgm.shape}")
-    print(f"All Image Data: {all_img_data.shape}")
-    print(f"All Label Data: {all_label_arr.shape}")
-    train_dataset, val_dataset, test_dataset = split_dataset(
-        all_cgm_trace,
-        all_znorm_cgm,
-        all_img_data,
-        all_a1c,
-        all_fasting_glucose,
-        all_label_arr,
-    )
+    print(f"All 24H Features: {time_series_trace_df.shape}")
+    print(f"All meal event Data: {meal_event_df.shape}")
+    heatmap_fnames = np.array(heatmap_fnames)
 
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    torch.save(train_dataset, f"{save_dir}cgmacros_train_dataset.pt")
-    torch.save(val_dataset, f"{save_dir}cgmacros_val_dataset.pt")
-    torch.save(test_dataset, f"{save_dir}cgmacros_test_dataset.pt")
+    time_series_trace_df.to_csv(f"{save_dir}cgmacros_time_series_trace.csv")
+    meal_event_df.to_csv(f"{save_dir}cgmacros_meal_event.csv")
     print(f"Saved to {save_dir}")
-    return train_dataset, val_dataset, test_dataset
+    return time_series_trace_df, meal_event_df, heatmap_fnames
 
 
 def main():
-    # load_CGMacros()
-    generate_CGMacrosDataset()
+    generate_24H_CGMacros_dataset()
 
 
 if __name__ == "__main__":
